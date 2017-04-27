23bacd0608bab98b0761aaded9c2fedaacc0880c
HelloAmazingWorld <ericksu@Ericks-MacBook-Pro.local>
2017-04-27 13:10:24 -0700

    deploy

--- TwicketSegmentedControlDemo/TwicketSegmentedControlDemo/ViewController.swift
+++ TwicketSegmentedControlDemo/TwicketSegmentedControlDemo/ViewController.swift
@@ -14,14 +14,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        // The TwicketSegmentedControl
         let titles = ["First", "Second", "Third"]
         let frame = CGRect(x: 5, y: view.frame.height / 2 - 20, width: view.frame.width - 10, height: 40)
+        // set position
 
         let segmentedControl = TwicketSegmentedControl(frame: frame)
+        // initial the twicketSegment in the frame
         segmentedControl.setSegmentItems(titles)
         segmentedControl.delegate = self
+        //
 
         view.addSubview(segmentedControl)
+        // TwicketSegmentedControl End
+        
     }
 }
 
